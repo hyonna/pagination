c9504a2077c04a25178963682e103de9020bb143
hyonna <hyonna@yunhyeon-aui-MacBookPro.local>
2022-12-07 18:18:07 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Pagination from './components/Pagination';
+
+interface Airline {
+  id: number;
+  name: string;
+  country: string;
+  logo: string;
+  slogen: string;
+  head_quaters: string;
+  website: string;
+  established: string;
+}
+
+interface Passenger {
+  _id: string;
+  name: string;
+  trips: number;
+  airline: Airline;
+  __v: number;
+}
+
+interface Response {
+  totalPassengers: number;
+  totalPages: number;
+  data: Array<Passenger>;
+}
 
 function App() {
+
+  const [page, setPage] = useState(0);
+  const [totalPages, setTotalPages] = useState(0);
+  const [items, setItems] = useState<Array<Passenger>>([]);
+
+  const handlePageChange = (currentPage: number): void => {
+    setPage(currentPage);
+  }
+
+  useEffect(() => { 
+
+    const fetch =async () => {
+      const params = { page, size: 10 };
+      const { data: {totalPages, data }} = await axios.get<Response>("https://api.instantwebtools.net/v1/passenger", { params });
+      setTotalPages(totalPages);
+      setItems(data);
+    }
+
+    fetch();
+
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {
+          items.map(item => (
+            <li key={item._id}>{item.name}</li>
+          ))
+        }
+      </ul>
+      <Pagination count={totalPages} page={page} onPageChange={handlePageChange} /> 
     </div>
   );
 }
